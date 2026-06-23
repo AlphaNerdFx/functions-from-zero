1b61cf1ea364bdaf34f9accacca4619e05e4e188
Youssef Larbi <yousseflarbi2023@gmail.com>
2026-06-23 17:39:40 +0000

    adding tests for cli

---
+++ wikibot.py
@@ -0,0 +1,12 @@
+import click
+from mylib.bot import scrape
+
+@click.command()
+@click.option('--name', help='Web page you want to scrape')
+@click.option('--length', help='The length of the output from wikipedia')
+def cli(name, length):
+    result = scrape(name, length)
+    click.echo(click.style(f'{result}'))
+
+if __name__ == '__main__':
+    cli()
